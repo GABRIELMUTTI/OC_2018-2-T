0d1d708d88e39d249430ab70a9d0c3f01b43a3cb
GABRIELMUTTI <gabrielmuttiteixeira@gmail.com>
2018-11-24 15:03:23 -0200

    Implemented test function.

--- include/test/test_sort_weights.h
+++ include/test/test_sort_weights.h
@@ -1,6 +1,16 @@
 #ifndef __TEST_SORT_WEIGHTS_H__
 #define __TEST_SORT_WEIGHTS_H__
 
+#include "../instance.h"
+#include "../instance_loader.h"
+#include "../grasp.h"
+
+#define TST_SUCCESS 0
+#define TST_ERR_LOAD_INSTANCE -1
+#define TST_ERR_SORT_WEIGHTS -2
+#define TST_ERR_NOT_SORTED -3
+#define TST_ERR_WRONG_VERTEX_WEIGHT -4
+
 int testSortWeights();
 
 
--- src/test/test_sort_weights.c
+++ src/test/test_sort_weights.c
@@ -2,6 +2,41 @@
 
 int testSortWeights()
 {
+    const float EPSILON = 1e-10;
+    
+    Instance* instance;
 
+    if (loadInstance(&instance, "home/Documents/ufrgs/courses/oc/trab/instances/cmb01") != 0)
+    {
+	return TST_ERR_LOAD_INSTANCE;
+    }
+
+    VertexWeight* weights;
+
+    if (sortWeights(instance, &weights) != 0)
+    {
+	return TST_ERR_SORT_WEIGHTS;
+    }
+
+    // Checks if its sorted.
+    unsigned int i;
+    for (i = 0; i < instance->numVertices - 1; i++)
+    {
+	if (weights[i].weight < weights[i + 1].weight)
+	{
+	    return TST_ERR_NOT_SORTED;
+	}
+
+	if (fabs(weights[i].weight - instance->weights[weights[i].vertex]) > EPSILON)
+	{
+	    return TST_ERR_WRONG_VERTEX_WEIGHT;
+	}
+    }
+    
     return 0;
+}
+
+int main()
+{
+    return testSortWeights();
 }
