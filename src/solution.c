e8fb5b36ea088dabfe1d456413041dcd8f928891
GABRIELMUTTI <gabrielmuttiteixeira@gmail.com>
2018-11-26 23:59:23 -0200

    Fixed coloration malloc size.

--- src/solution.c
+++ src/solution.c
@@ -5,7 +5,7 @@ int newSolution(Solution** solution, Instance* instance)
     *solution = malloc(sizeof(Solution));
     if (*solution == NULL) { return ERR_MALLOC; }
 
-    (*solution)->coloration = malloc(sizeof(unsigned int*) * instance->numColors);
+    (*solution)->coloration = malloc(sizeof(unsigned int) * instance->numVertices);
     if ((*solution)->coloration == NULL) { return ERR_MALLOC; }
 
     (*solution)->numVertexPerColor = malloc(sizeof(unsigned int) * instance->numColors);
